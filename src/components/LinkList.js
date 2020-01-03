bdd64f879c64e3ef610a49154fb226f96db4ba4b
juzhiyuan <jjzhiyuan@gmail.com>
2020-01-03 12:14:13 +0800

    feat: added Cache

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,7 +3,7 @@ import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
 import Link from './Link'
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
     feed {
       links {
@@ -11,12 +11,30 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
 `
 
 class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY })
+    const votedLink = data.feed.links.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+
+    store.writeQuery({ query: FEED_QUERY, data })
+  }
+
   render() {
     return (
       <Query query={FEED_QUERY}>
@@ -33,7 +51,7 @@ class LinkList extends Component {
             const linksToRender = data.feed.links
             return (
               <div>
-                {linksToRender.map(link => <Link key={link.id} link={link} />)}
+                {linksToRender.map((link, index) => <Link key={link.id} link={link} index={index} updateCacheAfterVote={this._updateCacheAfterVote} />)}
               </div>
             )
           }
